b1cbde9c5255c0964fac72314b45c832f5d15892
Kashish Vaish <kashishvaish.kv@gmail.com>
2022-08-03 13:51:25 +0530

    Fixed errors

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 const email = document.getElementById("email");
 
+const dob = document.getElementById("dob");
+const date = new Date(dob.value);
+var minValidDob = new Date();
+minValidDob.setFullYear(minValidDob.getFullYear() - 55);
+var dd = minValidDob.getDate();
+var mm = minValidDob.getMonth() + 1;
+if (dd < 10) {
+  dd = "0" + dd;
+}
+if (mm < 10) {
+  mm = "0" + mm;
+}
+minValidDob = minValidDob.getFullYear() + "-" + mm + "-" + dd;
+
+var maxValidDob = new Date();
+maxValidDob.setFullYear(maxValidDob.getFullYear() - 18);
+dd = maxValidDob.getDate();
+mm = maxValidDob.getMonth() + 1;
+if (dd < 10) {
+  dd = "0" + dd;
+}
+if (mm < 10) {
+  mm = "0" + mm;
+}
+maxValidDob = maxValidDob.getFullYear() + "-" + mm + "-" + dd;
+document.getElementById("dob").min = minValidDob;
+document.getElementById("dob").max = maxValidDob;
+
 email.addEventListener("input", function (event) {
   if (email.validity.typeMismatch) {
     email.setCustomValidity("This is not a valid email address!");
@@ -9,27 +37,6 @@ email.addEventListener("input", function (event) {
   }
 });
 
-
-function ageValidate() {
-  const dob = document.getElementById("dob");
-  const date = new Date(dob.value);
-  const minValidDob = new Date();
-  minValidDob.setFullYear(minValidDob.getFullYear()-55);
-  const maxValidDob = new Date();
-  maxValidDob.setFullYear(maxValidDob.getFullYear()-18);
-  if (minValidDob.getTime() <= date.getTime()  && date.getTime() <= maxValidDob.getTime()) {
-    document.getElementById("dobError").innerHTML = "";
-    return true;
-  } else if (minValidDob.getTime() > date.getTime()) {
-    document.getElementById("dobError").innerHTML = "Age cannot be more than 55 years.";
-    return false;
-  } else {
-    document.getElementById("dobError").innerHTML = "Age cannot be less than 18 years.";
-    return false;
-  }
-};
-
-
 let userEntries = localStorage.getItem("user-entries");
 if (userEntries) {
   userEntries = JSON.parse(userEntries);
@@ -37,41 +44,36 @@ if (userEntries) {
   userEntries = [];
 }
 
-
 const displayEntries = () => {
-    const savedUserEntries = localStorage.getItem("user-entries");
-    let entries = "";
-    if (savedUserEntries) {
-        const parsedUserEntries = JSON.parse(savedUserEntries);
-        entries = parsedUserEntries
-        .map((entry) => {
-            const name = `<td>${entry.name}</td>`;
-            const email = `<td>${entry.email}</td>`;
-            const password = `<td>${entry.password}</td>`;
-            const dob = `<td>${entry.dob}</td>`;
-            const acceptTerms = `<td>${entry.acceptTermsAndConditions}</td>`;
-            const row = `<tr>${name} ${email} ${password} ${dob} ${acceptTerms}</tr>`;
-            return row;
-        })
-        .join("\n");
-    }
-    var table = `<table><tr>
+  const savedUserEntries = localStorage.getItem("user-entries");
+  let entries = "";
+  if (savedUserEntries) {
+    const parsedUserEntries = JSON.parse(savedUserEntries);
+    entries = parsedUserEntries
+      .map((entry) => {
+        const name = `<td>${entry.name}</td>`;
+        const email = `<td>${entry.email}</td>`;
+        const password = `<td>${entry.password}</td>`;
+        const dob = `<td>${entry.dob}</td>`;
+        const acceptTerms = `<td>${entry.acceptTermsAndConditions}</td>`;
+        const row = `<tr>${name} ${email} ${password} ${dob} ${acceptTerms}</tr>`;
+        return row;
+      })
+      .join("\n");
+  }
+  var table = `<table><tr>
         <th>Name</th>
         <th>Email</th>
         <th>Password</th>
         <th>DOB</th>
         <th>Accepted Terms</th>
         </tr>${entries} </table>`;
-    let details = document.getElementById("user-entries");
-    details.innerHTML = table;
+  let details = document.getElementById("user-entries");
+  details.innerHTML = table;
 };
-
 
 const saveUserForm = (event) => {
   event.preventDefault();
-  if (!ageValidate()) {
-    return
-  }
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
@@ -89,22 +91,21 @@ const saveUserForm = (event) => {
   localStorage.setItem("user-entries", JSON.stringify(userEntries));
   document.getElementById("notificationBody").innerHTML = "Added Successfully!";
   document.getElementById("notification").style.visibility = "visible";
-  const inputs = document.querySelectorAll('#name, #email, #password, #dob, #acceptTerms');
-  inputs.forEach(input => {
-    input.value = '';
+  const inputs = document.querySelectorAll(
+    "#name, #email, #password, #dob, #acceptTerms"
+  );
+  inputs.forEach((input) => {
+    input.value = "";
   });
   displayEntries();
 };
-
 
 let form = document.getElementById("user_form");
 form.addEventListener("submit", saveUserForm, true);
 displayEntries();
 
-
-const closeNotification = document.getElementById("notificationClose")
+const closeNotification = document.getElementById("notificationClose");
 
 closeNotification.addEventListener("click", function (event) {
   document.getElementById("notification").style.visibility = "hidden";
 });
-
